dc4da75fec77e56fabaafc2adb2362414d98dcbc
David <david.ng_etich@tufts.edu>
2019-01-12 16:31:14 -0500

    Add files via upload
    Worked on the execution of the instruction set for the virtual machine

---
+++ execute.h
@@ -0,0 +1,17 @@
+/*
+*
+*       execute.h
+*
+* Anahita Sethi and David Ngetich
+*
+*/
+#ifndef EXECUTE_INCLUDED
+#define EXECUTE_INCLUDED
+
+#include <stdio.h>
+#include <stdint.h>
+#include "stack.h"
+
+void run_um(struct segment *program);
+
+#endif
