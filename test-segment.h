69c31800d8bd81b6076ec677b08d7137007d012a
David <david.ng_etich@tufts.edu>
2019-01-23 22:01:30 -0500

    Add files via upload
    Test the segmentaion of the memory

---
+++ test-segment.h
@@ -0,0 +1,18 @@
+/*
+* Anahita Sethi and David Ngetich
+*
+*/
+
+#include <stdbool.h>
+#include "segment.h"
+
+#ifndef TEST_SEGMENT_INCLUDED
+#define TEST_SEGMENT_INCLUDED
+
+
+bool test_new_free(void);
+bool test_get_instruction(void);
+bool test_map_unmap(void);
+
+
+#endif
